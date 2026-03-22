c449fe16874f281ebb93b47ecaf8779e552a5e0d
Norhan-Emad <norhan3588@gmail.com>
2026-03-22 13:07:07 +0200

    .

--- js/signUp.js
+++ js/signUp.js
@@ -148,5 +148,5 @@ signUpButton.addEventListener("click" , function(){
 })
 
 backLogin.addEventListener('click', function(){
-  window.location.href = "../index.html";
+  window.location.href = "./index.html";
 })
