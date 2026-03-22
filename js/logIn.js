9c324cafa58097bffad6dfc6e333c05d323dbacb
Norhan-Emad <norhan3588@gmail.com>
2026-03-22 16:55:06 +0200

    done

--- js/logIn.js
+++ js/logIn.js
@@ -1,4 +1,5 @@
-var list = JSON.parse(localStorage.getItem("usersData"));
+var list = JSON.parse(localStorage.getItem("usersData")) || [];
+
 var email = document.getElementById("inputEmail");
 var password = document.getElementById("inputPassword");
 var logIn = document.getElementById("log");
@@ -13,32 +14,36 @@ logIn.addEventListener("click", function (e) {
 function submit() {
   var emailValue = email.value;
   var passwordValue = password.value;
-  for (i = 0; i < list.length; i++) {
+  var found = false;
+
+  for (var i = 0; i < list.length; i++) {
     if (
       emailValue == list[i].userEmail &&
       passwordValue == list[i].userPassword
     ) {
-      localStorage.setItem("isVisited", "true"); //
-      logIn.setAttribute("href", "./home.html");
-      alert.classList.add("d-none");
-    } else {
-      alert.classList.remove("d-none");
-      alert.innerHTML = "Incorrect email or password";
-      logIn.removeAttribute("href");
+      found = true;
+      break;
     }
+  }
+
+  if (found) {
+    localStorage.setItem("isVisited", "true");
+    logIn.setAttribute("href", "./home.html"); //
+    alert.classList.add("d-none");
+  } else {
+    alert.classList.remove("d-none");
+    alert.innerHTML = "Incorrect email or password";
+    logIn.removeAttribute("href");
   }
 }
 
+// 🔥 loading 
 if (!localStorage.getItem("isVisited")) {
-  //
-  // First time → show loader
   setTimeout(() => {
     document.getElementById("start").style.display = "none";
   }, 7000);
 
-  // Save that user has visited
   localStorage.setItem("isVisited", "true");
 } else {
-  // Not first time → hide loader immediately
   document.getElementById("start").style.display = "none";
 }
